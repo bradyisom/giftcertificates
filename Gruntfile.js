d7b87cbc02a92110a3f34bee3086e9aa583602d9
Brady Isom <bradyisom@gmail.com>
2013-12-21 22:15:15 -0700

    Added deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,7 +169,7 @@ module.exports = function (grunt) {
     less: {
       dist: {
         files: {
-          ".tmp/styles/main.css": "<%= yeoman.app %>/styles/main.less"
+          '.tmp/styles/main.css': '<%= yeoman.app %>/styles/main.less'
         }
       }
     },
@@ -182,7 +182,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            // '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
